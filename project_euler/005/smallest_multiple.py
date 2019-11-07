4ee277d5dc6f0728fc7331b5a20cac2d35caeb29
Yuto Otaguro <yotagur@gmail.com>
2019-11-07 01:41:51 -0600

    Implement sieve

--- project_euler/005/smallest_multiple.py
+++ project_euler/005/smallest_multiple.py
@@ -1,4 +1,26 @@
 # https://www.hackerrank.com/contests/projecteuler/challenges/euler005
 
+class Sieve:
+    array = []
+
+    def isPrime(self, num: int) -> bool:
+        if num > len(self.array):
+            self.array = [True] * (num + 1)
+
+            for i in range(4, num):
+                max_diff = int(num / i)
+
+                print(max_diff)
+
+                for j in range(1, max_diff):
+                    self.array[i * j] = False
+
+        return self.array[num]
+
+
 def solve(n: int) -> int:
-    return 0
+    r = list(range(1, n + 1))
+
+    s = Sieve()
+
+    s.isPrime(r[-1])
--- project_euler/005/test_smallest_multiple.py
+++ project_euler/005/test_smallest_multiple.py
@@ -4,8 +4,8 @@ from smallest_multiple import *
 
 class TestSmallsetMultiple(unittest.TestCase):
     def test_given(self):
-        self.assertEqual(3, solve(6))
-        self.assertEqual(10, solve(2520))
+        self.assertEqual(6, solve(3))
+        self.assertEqual(2520, solve(10))
 
 if __name__ == '__main__':
     unittest.main()

1c157459deddea99338d6c791962fce041ded220
Yuto Otaguro <yotagur@gmail.com>
2019-11-07 11:49:02 -0600

    Create base skeleton, finish name quantifier

---
+++ project_euler/022/names_scores.py
@@ -0,0 +1,10 @@
+
+def quantify_name(name: str, idx: int) -> int:
+    # Since a is 1
+    a_code = ord('A') - 1
+
+    name_total = 0
+    for letter in name:
+        name_total += ord(letter) - a_code
+
+    return name_total * idx

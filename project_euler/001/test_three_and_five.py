751a2ea6e404f8687ebce789f6384bd381b7dc8e
Yuto Otaguro <yotagur@gmail.com>
2019-11-07 23:14:39 -0600

    Create another case

--- project_euler/001/test_three_and_five.py
+++ project_euler/001/test_three_and_five.py
@@ -6,6 +6,8 @@ class TestThreeAndFive(unittest.TestCase):
     def test_given(self):
         self.assertEqual(23, solve_unoptimized(10))
 
+    def test_manual(self):
+        self.assertEqual(14, solve_unoptimized(9))
 
 if __name__ == '__main__':
     unittest.main()

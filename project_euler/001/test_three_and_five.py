1d916c75d6b00b8384ba206df17423d000d55e4f
Yuto Otaguro <yotagur@gmail.com>
2019-11-07 23:12:16 -0600

    Given tests pass

---
+++ project_euler/001/test_three_and_five.py
@@ -0,0 +1,12 @@
+import unittest
+
+from three_and_five import solve_unoptimized
+
+class TestThreeAndFive(unittest.TestCase):
+    def test_given(self):
+        self.assertEqual(23, solve_unoptimized(10))
+
+    def test_
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ project_euler/001/three_and_five.py
@@ -0,0 +1,25 @@
+
+def solve_unoptimized(n: int) -> int:
+    n -= 1
+
+    # find how many multiples of 3 there are
+    three_mod = n % 3
+    three_n = n - three_mod
+
+    multiples_of_three = three_n / 3
+
+    # Partial sum formula for 3 + 6 + n
+    three_sum = int((3 / 2) * multiples_of_three * (multiples_of_three + 1))
+
+    # find how many multiples of 5 there are
+    five_mod = n % 5
+    five_n = n - five_mod
+
+    multiples_of_five = five_n / 5
+
+    five_sum = int((5 / 2) * multiples_of_five * (multiples_of_five + 1))
+
+    return three_sum + five_sum
+
+if __name__ == '__main__':
+    print(solve_unoptimized(1000))

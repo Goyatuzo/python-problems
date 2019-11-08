0842c2cf73d11f6d4b72112ed28a04876cf8b0dd
Yuto Otaguro <yotagur@gmail.com>
2019-11-07 23:17:53 -0600

    More basic test cases

--- project_euler/001/test_three_and_five.py
+++ project_euler/001/test_three_and_five.py
@@ -6,7 +6,15 @@ class TestThreeAndFive(unittest.TestCase):
     def test_given(self):
         self.assertEqual(23, solve_unoptimized(10))
 
-    def test_manual(self):
+    def test_less_than_ten(self):
+        self.assertEqual(0, solve_unoptimized(1))
+        self.assertEqual(0, solve_unoptimized(2))
+        self.assertEqual(0, solve_unoptimized(3))
+        self.assertEqual(3, solve_unoptimized(4))
+        self.assertEqual(3, solve_unoptimized(5))
+        self.assertEqual(8, solve_unoptimized(6))
+        self.assertEqual(14, solve_unoptimized(7))
+        self.assertEqual(14, solve_unoptimized(8))
         self.assertEqual(14, solve_unoptimized(9))
 
 if __name__ == '__main__':

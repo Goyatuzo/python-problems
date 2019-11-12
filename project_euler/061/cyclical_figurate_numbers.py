c495ea01042215b28e9986eadb11ce761e8102d6
Yuto Otaguro <yotagur@gmail.com>
2019-11-11 18:41:45 -0600

    Add the polygonal formulas

--- project_euler/061/cyclical_figurate_numbers.py
+++ project_euler/061/cyclical_figurate_numbers.py
@@ -1,3 +1,21 @@
+def triangle(n: int) -> int:
+    return n * (n + 1) / 2
+
+def square(n: int) -> int:
+    return n ** 2
+
+def pentagonal(n: int) -> int:
+    return n * (3 * n - 1) / 2
+
+def hexagonal(n: int) -> int:
+    return n * (2 * n - 1)
+
+def heptagonal(n: int) -> int:
+    return n * (5 * n - 3) / 2
+
+def octagonal(n: int) -> int:
+    return n * (3 * n - 2)
+
 def solve_first(n: int) -> int:
 
     # Triangle      45 < n < 140
@@ -5,5 +23,8 @@ def solve_first(n: int) -> int:
     # Pentagonal    26 < n < 81
     # Hexagonal     23 < n < 70
     # Heptagonal    20 < n < 63
-    # Octogonal     18 < n < 58
+    # Octagonal     18 < n < 58
+
+
+
     return 0

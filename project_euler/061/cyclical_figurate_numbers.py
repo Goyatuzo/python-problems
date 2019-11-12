756481be9c6566944e082c779fef6a76b4cafdff
Yuto Otaguro <yotagur@gmail.com>
2019-11-11 18:43:41 -0600

    Move comments to relevant areas

--- project_euler/061/cyclical_figurate_numbers.py
+++ project_euler/061/cyclical_figurate_numbers.py
@@ -1,30 +1,32 @@
 def triangle(n: int) -> int:
+    """Triangle 45 < n < 140"""
     return n * (n + 1) / 2
 
+
 def square(n: int) -> int:
+    """Square 32 < n < 99"""
     return n ** 2
 
+
 def pentagonal(n: int) -> int:
+    """Pentagonal 26 < n < 81"""
     return n * (3 * n - 1) / 2
 
+
 def hexagonal(n: int) -> int:
+    """Hexagonal 23 < n < 70"""
     return n * (2 * n - 1)
 
+
 def heptagonal(n: int) -> int:
+    """Heptagonal 20 < n < 63"""
     return n * (5 * n - 3) / 2
 
+
 def octagonal(n: int) -> int:
+    """Octagonal 18 < n < 58"""
     return n * (3 * n - 2)
 
+
 def solve_first(n: int) -> int:
-
-    # Triangle      45 < n < 140
-    # Square        32 < n < 99
-    # Pentagonal    26 < n < 81
-    # Hexagonal     23 < n < 70
-    # Heptagonal    20 < n < 63
-    # Octagonal     18 < n < 58
-
-
-
     return 0

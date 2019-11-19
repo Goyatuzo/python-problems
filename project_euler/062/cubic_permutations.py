ce93504579180a8b9ce2a625488cdf1c7aa75460
Yuto Otaguro <yotagur@gmail.com>
2019-11-18 18:06:14 -0600

    All permutations of number computed

--- project_euler/062/cubic_permutations.py
+++ project_euler/062/cubic_permutations.py
@@ -1,3 +1,39 @@
 
+from sys import getrecursionlimit
+
+def swap(s: str, start: int, end: int):
+    s[start], s[end] = s[end], s[start]
+    return s
+
+
+def permute_numbers(s: int):
+    s = str(s)
+    if len(s) == 0:
+        return [s]
+
+    permutations = []
+
+    for idx, char in enumerate(s):
+        temp_str = s[:idx] + s[idx + 1:]
+
+        print(temp_str)
+        rest = permute_numbers(temp_str)
+
+        for i in range(len(rest)):
+            rest[i] = char + rest[i]
+
+        permutations = permutations + rest
+
+    return permutations
+
+
 def solve_first(n):
-    return 0
+    found = False
+    n = 0
+    while not found:
+        n += 1
+        curr = n ** 3
+
+
+if __name__ == "__main__":
+    print(permute_numbers(123))

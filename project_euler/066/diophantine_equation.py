40afd111519109231d2ffd391713d3dfeb2f7d66
Yuto Otaguro <yotagur@gmail.com>
2019-11-18 20:20:33 -0600

    Works for test case

--- project_euler/066/diophantine_equation.py
+++ project_euler/066/diophantine_equation.py
@@ -1,8 +1,30 @@
 from math import sqrt
 
+
 def valid_d(max_d: int):
     return [d for d in range(max_d + 1) if not sqrt(d).is_integer()]
 
+
 def solve_first(max_d: int):
-    print(valid_d(max_d))
-    return 0
+
+    max_x = 0
+    highest_d = 0
+
+    for d in valid_d(max_d):
+        x = 2
+
+        y_squared = ((x ** 2) - 1) / d
+
+        while not sqrt(y_squared).is_integer():
+            x += 1
+            y_squared = ((x ** 2) - 1) / d
+
+        if x > max_x:
+            max_x = x
+            highest_d = d
+
+    return highest_d
+
+
+if __name__ == "__main__":
+    print(solve_first(1000))

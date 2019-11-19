9e7a7e2e9b9996d3bf2b803149ed6472df8f0701
Yuto Otaguro <yotagur@gmail.com>
2019-11-18 20:10:26 -0600

    Create valid_d function

--- project_euler/066/diophantine_equation.py
+++ project_euler/066/diophantine_equation.py
@@ -1,2 +1,8 @@
+from math import sqrt
+
+def valid_d(max_d: int):
+    return [d for d in range(max_d + 1) if not sqrt(d).is_integer()]
+
 def solve_first(max_d: int):
+    print(valid_d(max_d))
     return 0
--- project_euler/066/test_diophantine_equation.py
+++ project_euler/066/test_diophantine_equation.py
@@ -1,9 +1,12 @@
 import unittest
 
-from diophantine_equation import solve_first
+from diophantine_equation import solve_first, valid_d
 
 
 class TestDiophantineEquation(unittest.TestCase):
+    def test_valid_d(self):
+        self.assertEqual([2, 3, 5, 6, 7], valid_d(7))
+
     def test_given(self):
         self.assertAlmostEqual(5, solve_first(7))
 

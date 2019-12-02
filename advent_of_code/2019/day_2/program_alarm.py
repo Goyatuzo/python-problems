7db67cea3db4c55deb95ea07497a1cdcb37a602c
Yuto Otaguro <yotagur@gmail.com>
2019-12-02 02:07:07 -0600

    Kind of completed

--- advent_of_code/2019/day_2/program_alarm.py
+++ advent_of_code/2019/day_2/program_alarm.py
@@ -1,8 +1,41 @@
 
 def solve_first(ints):
-    
+    for curr in range(0, len(ints), 4):
+        op = ints[curr]
 
-    return 0
+        if op == 99:
+            break
+
+        idx_one = ints[curr + 1]
+        idx_two = ints[curr + 2]
+        idx_three = ints[curr + 3]
+
+        # Check if any are 99
+        if idx_one == 99 or idx_two == 99 or idx_three == 99:
+            break
+
+        val_one = ints[idx_one]
+        val_two = ints[idx_two]
+
+        if op == 1:
+            updated = val_one + val_two
+        elif op == 2:
+            updated = val_one * val_two
+
+        # Update destination with new value
+        ints[idx_three] = updated
+
+    return ints
+
 
 if __name__ == "__main__":
-    pass
+    part_one_fuel = 0
+    part_two_fuel = 0
+
+    with open('input.txt', 'r') as f:
+        line = f.readline()
+
+        values = [int(i) for i in line.split(',')]
+        print(values)
+
+        print('Part 1: ' + ','.join([str(i) for i in solve_first(values)]))
--- advent_of_code/2019/day_2/test_program_alarm.py
+++ advent_of_code/2019/day_2/test_program_alarm.py
@@ -12,6 +12,9 @@ class TestProgramAlarm(unittest.TestCase):
         self.assertEqual([30, 1, 1, 4, 2, 5, 6, 0, 99],
                          solve_first([1, 1, 1, 4, 99, 5, 6, 0, 99]))
 
+        self.assertEqual([3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50], solve_first(
+            [1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50]))
+
 
 if __name__ == '__main__':
     unittest.main()

b54ae2b9558a50a9a5bb96783d8dabd5438f9a34
Yuto Otaguro <yotagur@gmail.com>
2019-12-04 15:51:50 -0600

    Part 2 contains same answers as part 1

--- advent_of_code/2019/day_4/test_secure_container.py
+++ advent_of_code/2019/day_4/test_secure_container.py
@@ -1,9 +1,18 @@
 import unittest
 
-from secure_container import meets_criteria_two
+from secure_container import solve_first, solve_second, meets_criteria_two
 
 
 class TestSecureContainer(unittest.TestCase):
+
+    def test_part_two_subset_of_one(self):
+        lo = 278384
+        hi = 824795
+
+        first = solve_first(lo, hi)
+        second = solve_second(lo, hi)
+
+        self.assertTrue(set(second).issubset(set(first)))
 
     def test_part_two_given(self):
         self.assertEqual(True, meets_criteria_two(777777))

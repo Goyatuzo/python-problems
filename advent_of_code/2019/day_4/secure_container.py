5cf9e3a0f51ee8ef34f51de13b71f02d13ddc640
Yuto Otaguro <yotagur@gmail.com>
2019-12-04 15:10:50 -0600

    Second attempt at part 2

--- advent_of_code/2019/day_4/secure_container.py
+++ advent_of_code/2019/day_4/secure_container.py
@@ -19,11 +19,59 @@ def meets_criteria(num):
     return adjacent_digits and is_increasing
 
 
+def meets_criteria_two(num):
+    # Cast to string for easier processing
+    num = str(num)
+
+    # Keep track of results
+    adjacent_count = 0
+    adjacent_digits = False
+
+    is_increasing = True
+
+    previous_digit = num[0]
+
+    for digit in num[1:]:
+        if digit == previous_digit:
+            adjacent_count += 1
+            adjacent_digits = True
+        else:
+            if adjacent_count == 0:
+                pass
+            elif adjacent_count % 2 != 0:
+                adjacent_count = 0
+            elif adjacent_count < 2:
+                adjacent_count = -1
+                break
+
+        if int(previous_digit) > int(digit):
+            is_increasing = False
+
+        previous_digit = digit
+
+    result = adjacent_count != -1 and adjacent_count % 2 != 0 and adjacent_digits and is_increasing
+
+    if result:
+        print("NUM: " + num + ", ADJ: " + str(adjacent_count))
+
+    return result
+
+
 def solve_first(lo, hi):
     values = [i for i in range(lo, hi + 1) if meets_criteria(i)]
-    
+
+    return values
+
+
+def solve_second(lo, hi):
+    values = [i for i in range(lo, hi + 1) if meets_criteria_two(i)]
+
     return values
 
 
 if __name__ == "__main__":
-    print("Part 1: " + str(len(solve_first(278384, 824795))))
+    lo = 278384
+    hi = 824795
+
+    print("Part 1: " + str(len(solve_first(lo, hi))))
+    print("Part 2: " + str(len(solve_second(lo, hi))))

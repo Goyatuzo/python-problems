7b84b3651e916e22354133497c8b8fa93c8feea1
Yuto Otaguro <yotagur@gmail.com>
2019-12-05 11:59:51 -0600

    Create base files

---
+++ advent_of_code/2019/day_5/test_chance_of_asteroids.py
@@ -0,0 +1,12 @@
+import unittest
+
+from chance_of_asteroids import solve_first
+
+
+class TestChanceOfAsteroids(unittest.TestCase):
+    def test_part_one(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()

7be9e96a42c38311183e031a255e1bccbc2bbe5d
Yuto Otaguro <yotagur@gmail.com>
2019-12-05 13:45:23 -0600

    Update unit tests

--- advent_of_code/2019/day_5/test_chance_of_asteroids.py
+++ advent_of_code/2019/day_5/test_chance_of_asteroids.py
@@ -1,11 +1,19 @@
 import unittest
 
-from chance_of_asteroids import solve_first
+from chance_of_asteroids import process_opcodes
 
 
 class TestChanceOfAsteroids(unittest.TestCase):
-    def test_part_one(self):
-        pass
+    def test_from_program_alarm(self):
+        self.assertEqual([2, 0, 0, 0, 99], process_opcodes([1, 0, 0, 0, 99]))
+        self.assertEqual([2, 3, 0, 6, 99], process_opcodes([2, 3, 0, 3, 99]))
+        self.assertEqual([2, 4, 4, 5, 99, 9801],
+                         process_opcodes([2, 4, 4, 5, 99, 0]))
+        self.assertEqual([30, 1, 1, 4, 2, 5, 6, 0, 99],
+                         process_opcodes([1, 1, 1, 4, 99, 5, 6, 0, 99]))
+
+        self.assertEqual([3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50], process_opcodes(
+            [1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50]))
 
 
 if __name__ == '__main__':

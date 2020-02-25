1e775c2f757f7beb8e389b4b07516e8c31b70759
Yuto Otaguro <yotagur@gmail.com>
2020-02-24 19:24:30 -0600

    Add the other test case

--- hackerrank/Practice/Algorithms/Search/ice_cream_parlor/test_ice_cream_parlor.py
+++ hackerrank/Practice/Algorithms/Search/ice_cream_parlor/test_ice_cream_parlor.py
@@ -8,6 +8,11 @@ class TestIceCreamParlor(unittest.TestCase):
         self.assertEqual([1, 4], icecreamParlor(4, [1, 4, 5, 3, 2]))
         self.assertEqual([1, 2], icecreamParlor(4, [2, 2, 4, 3]))
 
+    def test_given_2(self):
+        self.assertEqual([2, 4], icecreamParlor(9, [1, 3, 4, 6, 7, 9]))
+        self.assertEqual([3, 4], icecreamParlor(8, [1, 3, 4, 4, 6, 8]))
+        self.assertEqual([1, 2], icecreamParlor(3, [1, 2]))
+
 
 if __name__ == '__main__':
     unittest.main()

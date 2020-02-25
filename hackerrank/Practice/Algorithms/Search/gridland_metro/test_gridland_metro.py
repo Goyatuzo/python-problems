123e198104a83f2883f4e89aa5ae3f9e0ae7a26d
Yuto Otaguro <yotagur@gmail.com>
2020-02-24 21:35:20 -0600

    Update test

--- hackerrank/Practice/Algorithms/Search/gridland_metro/test_gridland_metro.py
+++ hackerrank/Practice/Algorithms/Search/gridland_metro/test_gridland_metro.py
@@ -4,6 +4,9 @@ from gridland_metro import gridlandMetro
 
 
 class TestGridlandMetro(unittest.TestCase):
+    def test_problem(self):
+        self.assertEqual(5, gridlandMetro(4, 4, 4, [[1, 1, 4], [2, 2, 4], [3, 1, 2], [4, 2, 3]]))
+
     def test_given(self):
         self.assertEqual(9, gridlandMetro(4, 4, 3, [[2, 2, 3], [3, 1, 4], [4, 4, 4]]))
 

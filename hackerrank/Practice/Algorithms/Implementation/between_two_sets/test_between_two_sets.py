cbb28296cae7b41cc7d5f55ef5d00fa4b6f6cecf
Yuto Otaguro <yotagur@gmail.com>
2020-04-27 17:27:53 -0500

    Unit test runs

--- hackerrank/Practice/Algorithms/Implementation/between_two_sets/test_between_two_sets.py
+++ hackerrank/Practice/Algorithms/Implementation/between_two_sets/test_between_two_sets.py
@@ -1,7 +1,10 @@
 import unittest
 
+from between_two_sets import getTotalX
+
 class TestBetweenTwoPoints(unittest.TestCase):
     def test_given(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()

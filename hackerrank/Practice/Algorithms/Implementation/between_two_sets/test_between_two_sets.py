fae0642439e1f33ff655ca0df1197495f8077b82
Yuto Otaguro <yotagur@gmail.com>
2020-04-27 17:44:42 -0500

    Test files now accurate

--- hackerrank/Practice/Algorithms/Implementation/between_two_sets/test_between_two_sets.py
+++ hackerrank/Practice/Algorithms/Implementation/between_two_sets/test_between_two_sets.py
@@ -4,7 +4,7 @@ from between_two_sets import getTotalX
 
 class TestBetweenTwoPoints(unittest.TestCase):
     def test_given(self):
-        pass
+        self.assertEqual(3, getTotalX([2, 4], [16, 32, 96]))
 
 if __name__ == '__main__':
     unittest.main()

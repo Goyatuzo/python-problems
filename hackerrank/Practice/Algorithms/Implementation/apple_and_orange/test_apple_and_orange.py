e30f2638c09a17da352be5ed4d6ec29e2c76fbd8
Yuto Otaguro <yotagur@gmail.com>
2020-04-28 02:51:55 -0500

    Add base files

---
+++ hackerrank/Practice/Algorithms/Implementation/apple_and_orange/test_apple_and_orange.py
@@ -0,0 +1,12 @@
+import unittest
+
+from apple_and_orange import getTotalX
+
+class TestAppleAndOrange(unittest.TestCase):
+    def test_given(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
+

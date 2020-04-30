ffe147bb3fa442ed7e146996d38e16a333c695e3
Yuto Otaguro <yotagur@gmail.com>
2020-04-30 15:18:07 -0500

    Code works for submission

--- hackerrank/Practice/Algorithms/Search/hackerland_radio_transmitters/hackerland_radio_transmitters.py
+++ hackerrank/Practice/Algorithms/Search/hackerland_radio_transmitters/hackerland_radio_transmitters.py
@@ -6,9 +6,31 @@ import random
 import re
 import sys
 
+from typing import List
+
 # Complete the hackerlandRadioTransmitters function below.
-def hackerlandRadioTransmitters(x, k):
-    pass
+def hackerlandRadioTransmitters(x: List[int], k: int):
+    transmitters = []
+    
+    x.sort()
+
+    start_pt = None
+    for pt in x:
+        # If there is no start point, set it as the current point
+        if start_pt is None:
+            start_pt = pt
+        # We found a location to place
+        if pt == start_pt + k:
+            start_pt = None
+            transmitters.append(pt)
+        elif pt > start_pt + k:
+            start_pt = None
+            transmitters.append(pt)
+            transmitters.append(start_pt)
+
+    print(transmitters)
+    return transmitters
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
@@ -21,7 +43,7 @@ if __name__ == '__main__':
 
     x = list(map(int, input().rstrip().split()))
 
-    result = hackerlandRadioTransmitters(x, k)
+    result = len(hackerlandRadioTransmitters(x, k))
 
     fptr.write(str(result) + '\n')
 

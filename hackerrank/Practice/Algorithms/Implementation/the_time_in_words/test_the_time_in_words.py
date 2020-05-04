3c0e041f3d52be93af820447b1dc5a0135a85921
Yuto Otaguro <yotagur@gmail.com>
2020-05-04 15:11:53 -0500

    Create files

---
+++ hackerrank/Practice/Algorithms/Implementation/the_time_in_words/test_the_time_in_words.py
@@ -0,0 +1,11 @@
+import unittest
+
+from the_time_in_words import timeInWords
+
+class TestTheTimeInWords(unittest.TestCase):
+    def test_given(self):
+        self.assertEqual('thirteen minutes to six', timeInWords(5, 47))
+        self.assertEqual('three \'o clock', timeInWords(3, 0))
+
+if __name__ == '__main__':
+    unittest.main()

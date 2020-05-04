f601039c203a5d31181b676ba86d4db06c3d329e
Yuto Otaguro <yotagur@gmail.com>
2020-05-04 15:17:21 -0500

    Create dict array

--- hackerrank/Practice/Algorithms/Implementation/the_time_in_words/test_the_time_in_words.py
+++ hackerrank/Practice/Algorithms/Implementation/the_time_in_words/test_the_time_in_words.py
@@ -6,6 +6,7 @@ class TestTheTimeInWords(unittest.TestCase):
     def test_given(self):
         self.assertEqual('thirteen minutes to six', timeInWords(5, 47))
         self.assertEqual('three \'o clock', timeInWords(3, 0))
+        self.assertEqual('quarter past seven', timeInWords(7, 15))
 
 if __name__ == '__main__':
     unittest.main()

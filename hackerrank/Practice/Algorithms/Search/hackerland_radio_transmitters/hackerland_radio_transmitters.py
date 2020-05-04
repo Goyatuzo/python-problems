96004de1ce368adc0d991e6dbf90bf11c0ce5bca
Yuto Otaguro <yotagur@gmail.com>
2020-05-04 00:34:59 -0500

    Partial

--- hackerrank/Practice/Algorithms/Search/hackerland_radio_transmitters/hackerland_radio_transmitters.py
+++ hackerrank/Practice/Algorithms/Search/hackerland_radio_transmitters/hackerland_radio_transmitters.py
@@ -10,23 +10,21 @@ from typing import List
 
 # Complete the hackerlandRadioTransmitters function below.
 def hackerlandRadioTransmitters(x: List[int], k: int):
-    transmitters = []
-    
+    transmitters = [] 
     x.sort()
 
     start_pt = None
     for pt in x:
-        # If there is no start point, set it as the current point
         if start_pt is None:
             start_pt = pt
-        # We found a location to place
-        if pt == start_pt + k:
-            start_pt = None
-            transmitters.append(pt)
-        elif pt > start_pt + k:
-            start_pt = None
-            transmitters.append(pt)
+            continue
+
+        if start_pt + 2 * k > pt:
             transmitters.append(start_pt)
+            start_pt = pt
+        elif start_pt + 2 * k == pt:
+            transmitters.append(start_pt)
+            start_pt = None
 
     print(transmitters)
     return transmitters
--- hackerrank/Practice/Algorithms/Search/hackerland_radio_transmitters/test_hackerland_radio_transmitters.py
+++ hackerrank/Practice/Algorithms/Search/hackerland_radio_transmitters/test_hackerland_radio_transmitters.py
@@ -3,6 +3,13 @@ import unittest
 from hackerland_radio_transmitters import hackerlandRadioTransmitters
 
 class TestHackerlandRadioTransmitters(unittest.TestCase):
+    def test_all_spread(self):
+        self.assertEqual(5, len(hackerlandRadioTransmitters([1, 3, 5, 7, 9],
+            1)))
+        self.assertEqual(3, len(hackerlandRadioTransmitters([2, 3, 5, 7, 8, 9],
+            1)))
+        self.assertEqual(2, len(hackerlandRadioTransmitters([1, 4, 5, 6], 1)))
+
     def test_problem(self):
         self.assertEqual(3, len(hackerlandRadioTransmitters([1, 2, 3, 5, 9],
             1)))

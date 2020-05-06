b33ba54d55baf7c4027619318e6c8666b7a29684
Yuto Otaguro <yotagur@gmail.com>
2020-05-06 06:34:05 +0000

    Euler 14 (#26)
    * Replace cpp with py
    
    * Given cases pass
    
    * Minimal performance improvements
    
    * try improvement by typecasting
    
    * Inefficiently working
    
    * Remove print statements

---
+++ project_euler/014/longest_collatz.py
@@ -0,0 +1,57 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+cache = {
+    1: 1
+}
+
+def next_chain(n: int) -> int:
+    if n % 2 == 0:
+        return n / 2
+    else:
+        return 3 * n + 1
+
+def longest_collatz(n: int) -> int:
+    maximum = 0
+    maximum_num = 0
+    for i in range(2, n + 1):
+        l = 0
+        curr = i
+        chain = []
+
+        while curr != 1:
+            chain.append(curr)
+
+            if curr in cache:
+                l += cache[curr]
+                break
+            else:
+                curr = next_chain(curr)
+                l += 1
+    
+        if chain[-1] in cache:
+            length = cache[chain[-1]]
+        else:
+            length = 1
+
+        for val in chain[::-1]:
+            cache[int(val)] = length
+            length += 1
+
+        if l >= maximum:
+            maximum_num = i
+            maximum = l
+
+
+        # print(f'i: {i}, max: {maximum_num}, length of i: {l}')
+
+    return maximum_num
+
+
+if __name__ == '__main__':
+    print(longest_collatz(1000000))
---
+++ project_euler/014/test_longest_collatz.py
@@ -0,0 +1,20 @@
+import unittest
+
+from longest_collatz import longest_collatz
+
+class TestLongestCollatz(unittest.TestCase):
+    def test_10(self):
+        self.assertEqual(9, longest_collatz(10))
+
+    def test_15(self):
+        self.assertEqual(9, longest_collatz(15))
+
+    def test_20(self):
+        self.assertEqual(19, longest_collatz(20))
+
+    def test_21(self):
+        self.assertEqual(1, longest_collatz(20))
+
+
+if __name__ == '__main__':
+    unittest.main()

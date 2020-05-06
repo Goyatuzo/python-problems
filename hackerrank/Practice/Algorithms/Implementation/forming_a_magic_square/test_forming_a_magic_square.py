d5f029d87272d9eceb9f90e07895d9c389b5be7c
Yuto Otaguro <yotagur@gmail.com>
2020-05-06 14:27:38 -0500

    Fix one of the tests

--- hackerrank/Practice/Algorithms/Implementation/forming_a_magic_square/test_forming_a_magic_square.py
+++ hackerrank/Practice/Algorithms/Implementation/forming_a_magic_square/test_forming_a_magic_square.py
@@ -4,7 +4,7 @@ from forming_a_magic_square import formingMagicSquare
 
 class TestBetweenTwoPoints(unittest.TestCase):
     def test_given(self):
-        self.assertEqual(set([4, 8, 16]), formingMagicSquare([[4, 8, 2], [4, 5,
+        self.assertEqual(4, formingMagicSquare([[4, 8, 2], [4, 5,
             7], [6, 1, 6]]))
 
 if __name__ == '__main__':

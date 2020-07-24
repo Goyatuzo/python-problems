c9920476c4384f55e8d647905f9f09b7b92452f9
Yuto Otaguro <yotagur@gmail.com>
2020-07-24 14:54:12 -0500

    Finish minimum loss

--- hackerrank/Practice/Algorithms/Search/minimum_loss/minimum_loss.py
+++ hackerrank/Practice/Algorithms/Search/minimum_loss/minimum_loss.py
@@ -8,30 +8,45 @@ import sys
 
 from typing import List
 
+# https://www.hackerrank.com/challenges/minimum-loss/problem
+
+
+def find_min(lst: List[int], thresh: int) -> int:
+    """Find the min in a given list and threshold. Assumes
+    the input list is sorted in ascending order."""
+    if len(lst) < 2:
+        return lst[0]
+
+    local_min = lst[1] - lst[0]
+    min_tup = (lst[0], lst[1])
+
+    for i in range(len(lst) - 1):
+        diff = lst[i + 1] - lst[i]
+        if diff > thresh and diff < local_min:
+            local_min = diff
+            min_tup = (lst[i], lst[i + 1])
+
+    return min_tup
+
+
 # Complete the minimumLoss function below.
 
-
 def minimumLoss(price: List[int]) -> int:
-    min_loss = -1
+    sorted_price = sorted(price)
 
-    for i in range(len(price) - 2):
-        # Find the max value of the valid entries
-        valid_vals = [val for val in price[i + 1:] if val < price[i]]
+    found = False
+    iter_count = 0
 
-        # If there are no valid values left, just skip
-        if len(valid_vals) == 0:
-            continue
+    while not found:
+        local_min = find_min(sorted_price, iter_count)
 
-        biggest = max([val for val in price[i + 1:] if val < price[i]])
-
-        diff = price[i] - biggest
-
-        if min_loss == -1:
-            min_loss = diff
+        # Verify the second number appears before the first
+        if price.index(local_min[1]) < price.index(local_min[0]):
+            return local_min[1] - local_min[0]
         else:
-            min_loss = min(diff, min_loss)
+            iter_count = local_min[1] - local_min[0]
 
-    return min_loss
+    return 0
 
 
 if __name__ == '__main__':

f339c0579a43c896c25f81ff283812e1727cbc99
Goyatuzo <yotagur@gmail.com>
2020-08-14 14:00:24 -0500

    Unit tests pass

--- code_forces/4/b/before_an_exam.py
+++ code_forces/4/b/before_an_exam.py
@@ -1,4 +1,27 @@
 from typing import List, Tuple
 
+
 def before_an_exam(days: int, total_time: int, reqs: List[Tuple[int, int]]) -> Tuple[str, str]:
-    pass
+    """
+    He has to study not less than minTime i and not more than maxTime i hours per each i-th day. 
+    """
+    times = ''
+    for tup in reqs:
+        if total_time == 0:
+            # Total time hit 0 before each requirement could be met, so
+            # he did not follow his parent's instructions.
+            return ['NO', '']
+        elif tup[1] < total_time:
+            # Subtract the largest amount of time possible from pool.
+            total_time -= tup[1]
+            times = times + f'{tup[1]} '
+        else:
+            # Total time only has itself remaining as the time
+            times = times + f'{total_time} '
+            total_time = 0
+
+    # If there is time left over, he did not follow his parent's instructions.
+    if total_time > 0:
+        return ['NO', '']
+
+    return ['YES', times]
--- code_forces/4/b/test_before_an_exam.py
+++ code_forces/4/b/test_before_an_exam.py
@@ -5,7 +5,7 @@ from before_an_exam import before_an_exam as foo
 class TestBeforeAnExam(unittest.TestCase):
     def test_given(self):
         self.assertEqual(['NO', ''], foo(1, 48, [(5, 7)]))
-        self.assertEqual('YES', foo(2, 5, [(0, 1), (3, 5)]))
+        self.assertEqual(['YES', '1 4 '], foo(2, 5, [(0, 1), (3, 5)]))
 
 if __name__ == '__main__':
     unittest.main()

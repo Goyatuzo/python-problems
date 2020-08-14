ff31d4f9755b41de479825a80bd52b34dfba7aa0
Goyatuzo <yotagur@gmail.com>
2020-08-14 17:38:10 -0500

    Write one more unit test just in case

--- code_forces/4/c/test_registration_system.py
+++ code_forces/4/c/test_registration_system.py
@@ -2,6 +2,7 @@ import unittest
 
 from registration_system import registration_system as foo
 
+# The dictionary is stored across the three test cases
 class TestRegistrationSystem(unittest.TestCase):
     def test_given(self):
         self.assertEqual('OK', foo('abacaba'))
@@ -16,6 +17,11 @@ class TestRegistrationSystem(unittest.TestCase):
         self.assertEqual('second1', foo('second'))
         self.assertEqual('OK', foo('third'))
         self.assertEqual('third1', foo('third'))
+    
+    def test_self(self):
+        self.assertEqual('OK', foo('testing'))
+        self.assertEqual('testing1', foo('testing'))
+        self.assertEqual('testing2', foo('testing'))
 
 if __name__ == '__main__':
     unittest.main()

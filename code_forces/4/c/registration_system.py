b5c7995aff4dbc9cbd547dddf9622b8870d042cf
Goyatuzo <yotagur@gmail.com>
2020-08-14 17:34:03 -0500

    Pass the first unit test

--- code_forces/4/c/registration_system.py
+++ code_forces/4/c/registration_system.py
@@ -1,2 +1,13 @@
+from typing import Dict
+
+db: Dict[str, int] = {}
+
 def registration_system(name: str) -> str:
-    pass
+    if name in db:
+        db[name] = db[name] + 1
+    else:
+        db[name] = 0
+        # If it's the first instance, return OK
+        return 'OK'
+
+    return f'{name}{db[name]}'

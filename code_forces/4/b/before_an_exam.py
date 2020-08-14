d67cbbf42ae72e41ee7469a96fdaf475aec9f2ad
Goyatuzo <yotagur@gmail.com>
2020-08-14 13:52:46 -0500

    Write function signature and tests

---
+++ code_forces/4/b/before_an_exam.py
@@ -0,0 +1,4 @@
+from typing import List, Tuple
+
+def before_an_exam(days: int, total_time: int, reqs: List[Tuple[int, int]]) -> Tuple[str, str]:
+    pass
---
+++ code_forces/4/b/test_before_an_exam.py
@@ -0,0 +1,11 @@
+import unittest
+
+from before_an_exam import before_an_exam as foo
+
+class TestBeforeAnExam(unittest.TestCase):
+    def test_given(self):
+        self.assertEqual(['NO', ''], foo(1, 48, [(5, 7)]))
+        self.assertEqual('YES', foo(2, 5, [(0, 1), (3, 5)]))
+
+if __name__ == '__main__':
+    unittest.main()

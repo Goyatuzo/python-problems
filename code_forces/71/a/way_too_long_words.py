f9b73149263e09714da5667ec97dd50c7a9bc754
Goyatuzo <yotagur@gmail.com>
2020-08-14 13:35:21 -0500

    First attempt

---
+++ code_forces/71/a/way_too_long_words.py
@@ -0,0 +1,15 @@
+
+def way_too_long_words(word: str) -> str:
+    """
+    Let's consider a word too long, if its length is strictly more than 10 characters.
+    All too long words should be replaced with a special abbreviation.
+
+    This abbreviation is made like this: we write down the first and the last letter
+    of a word and between them we write the number of letters between the first and
+    the last letters. That number is in decimal system and doesn't contain any leading
+    zeroes.
+    """
+    if len(word) > 10:
+        return word[0] + str(len(word) - 2) + word[-1]
+
+    return word

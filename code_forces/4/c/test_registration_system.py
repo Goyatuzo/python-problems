90a69f14d46c0a8a081b9b404ccab0a4822d721c
Goyatuzo <yotagur@gmail.com>
2020-08-14 17:35:04 -0500

    Write second set of unit tests

--- code_forces/4/c/test_registration_system.py
+++ code_forces/4/c/test_registration_system.py
@@ -8,6 +8,14 @@ class TestRegistrationSystem(unittest.TestCase):
         self.assertEqual('OK', foo('acaba'))
         self.assertEqual('abacaba1', foo('abacaba'))
         self.assertEqual('OK', foo('acab'))
+    
+    def test_given_two(self):
+        self.assertEqual('OK', foo('first'))
+        self.assertEqual('first1', foo('first'))
+        self.assertEqual('OK', foo('second'))
+        self.assertEqual('second1', foo('second'))
+        self.assertEqual('OK', foo('third'))
+        self.assertEqual('third1', foo('third'))
 
 if __name__ == '__main__':
     unittest.main()

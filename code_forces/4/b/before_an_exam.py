4ad3852a86586ce9eb3a8c63c8e3ac62a7a9af64
Yuto Otaguro <yotagur@gmail.com>
2020-08-14 17:24:16 -0500

    Cf 4b (#33)
    * Divide problem to subsets
    
    * WSL locks folders for some reason
    
    * Write function signature and tests
    
    * Unit tests pass
    
    * Last case should only trigger if min allows
    
    * Have the total time check run once at the end
    
    * Complete before an exam
    
    * Comments

---
+++ code_forces/4/b/before_an_exam.py
@@ -0,0 +1,54 @@
+from typing import List, Tuple
+
+
+def before_an_exam(days: int, total_time: int, reqs: List[Tuple[int, int]]) -> Tuple[str, str]:
+    """
+    He has to study not less than minTime i and not more than maxTime i hours per each i-th day.
+    """
+
+    # First let's try using all of max
+    prospect = [i[1] for i in reqs]
+
+    curr_sum = sum(prospect)
+
+    curr_i = len(prospect) - 1
+    while curr_sum > total_time and curr_i >= 0:
+        sum_diff = curr_sum - total_time
+        necessary = reqs[curr_i][1] - sum_diff
+
+        # If the current sum is too much but can be accounted for in the current day, do so
+        if necessary >= reqs[curr_i][0] and necessary <= reqs[curr_i][1]:
+            prospect[curr_i] = necessary
+
+            curr_sum += necessary
+            curr_sum -= reqs[curr_i][1]
+        # Otherwise he should study for the minimum time and go to yesterday
+        else:
+            prospect[curr_i] = reqs[curr_i][0]
+            # currsum can be recomputed in constant time since we have both values
+            curr_sum += reqs[curr_i][0]
+            curr_sum -= reqs[curr_i][1]
+
+        curr_i -= 1
+
+    if sum(prospect) != total_time:
+        return ['NO', '']
+
+    return ['YES', ' '.join([str(i) for i in prospect])]
+
+
+if __name__ == '__main__':
+    d, sumTime = map(int, input().split())
+
+    requirements = []
+
+    for _ in range(d):
+        mi, ma = map(int, input().split())
+
+        requirements.append((mi, ma))
+
+    res = before_an_exam(d, sumTime, requirements)
+
+    print(res[0])
+    if res[1] != '':
+        print(res[1])

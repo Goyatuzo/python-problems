da223e848caa2944467c86dc357eb2427c4c1025
Goyatuzo <yotagur@gmail.com>
2020-08-16 09:48:27 -0500

    Create file and test

--- code_forces/4/d/mysterious_present.py
+++ code_forces/4/d/mysterious_present.py
@@ -1,5 +1,8 @@
 
 from typing import List, Tuple
 
-def mysterious_present(n: int, w: int, h: int, vals: List[int]) -> Tuple[str]:
+def mysterious_present(n: int, w: int, h: int, vals: List[Tuple[int, int]]) -> Tuple[str]:
+    envelopes = [(i, env) for i, env in enumerate(vals) if env[0] > w and env[1] > h]
+
+    print(envelopes)
     pass

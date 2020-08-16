c7eb5823317fee581fea36cf17cfff135f1e1951
Goyatuzo <yotagur@gmail.com>
2020-08-16 10:18:15 -0500

    All tests pass

--- code_forces/1392/b/omkar_and_infinity_clock.py
+++ code_forces/1392/b/omkar_and_infinity_clock.py
@@ -1,5 +1,25 @@
 from typing import List
 
 
-def omkar_and_infinity_clock(k: int, nums: List[int]) -> List[int]:
-    pass
+def omkar_and_infinity_clock(k: int, nums: List[int]) -> str:
+    # Find the max
+    maximum = nums[0]
+
+    for i in nums:
+        maximum = i if i > maximum else maximum
+
+    # Now keep applying it while k > 0
+    while k > 0:
+        nums = [maximum - ai for ai in nums]
+
+        # Subtract 1 here since it's one operation
+        k -= 1
+
+        # Find max again
+        maximum = nums[0]
+        
+        for i in nums:
+            maximum = i if i > maximum else maximum
+
+        
+    return ' '.join(map(str, nums))
--- code_forces/1392/b/test_omkar_and_infinity_clock.py
+++ code_forces/1392/b/test_omkar_and_infinity_clock.py
@@ -7,6 +7,9 @@ class TestOmkarAndInfinityClock(unittest.TestCase):
         self.assertEqual('391 0', foo(1, [-199, 192]))
         self.assertEqual('0 6 1 3 5', foo(19, [5, -1, 4, 2, 0]))
         self.assertEqual('0', foo(2, [69]))
+    
+    def test_self(self):
+        self.assertEqual('0', foo(1, [50]))
 
 if __name__ == '__main__':
     unittest.main()

7b730bbb24ed2f259f89436ef087a8154a996d40
Goyatuzo <yotagur@gmail.com>
2020-08-16 10:11:47 -0500

    Infinity clock files

---
+++ code_forces/1392/b/omkar_and_infinity_clock.py
@@ -0,0 +1,5 @@
+from typing import List
+
+
+def omkar_and_infinity_clock(k: int, nums: List[int]) -> List[int]:
+    pass
---
+++ code_forces/1392/b/test_omkar_and_infinity_clock.py
@@ -0,0 +1,12 @@
+import unittest
+
+from omkar_and_infinity_clock import omkar_and_infinity_clock as foo
+
+class TestOmkarAndInfinityClock(unittest.TestCase):
+    def test_given(self):
+        self.assertEqual('391 0', foo(1, [-199, 192]))
+        self.assertEqual('0 6 1 3 5', foo(19, [5, -1, 4, 2, 0]))
+        self.assertEqual('0', foo(2, [69]))
+
+if __name__ == '__main__':
+    unittest.main()

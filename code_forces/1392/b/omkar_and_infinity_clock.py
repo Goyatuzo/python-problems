b6ed8be2a3314dea27355485ca02a285f2cb01e7
Goyatuzo <yotagur@gmail.com>
2020-08-16 10:58:08 -0500

    More optimized version

--- code_forces/1392/b/omkar_and_infinity_clock.py
+++ code_forces/1392/b/omkar_and_infinity_clock.py
@@ -2,27 +2,16 @@ from typing import List
 
 
 def omkar_and_infinity_clock(k: int, nums: List[int]) -> str:
-    # Find the max
-    maximum = nums[0]
+    # There are only two patterns that interleave
+    maximum = max(nums)
+    # If k is odd, it's easy
+    if k % 2 == 1:
+        return ' '.join([str(maximum - i) for i in nums])
+    else:
+        nums = [maximum - i for i in nums]
+        maximum = max(nums)
+        return ' ' .join([str(maximum - i) for i in nums])
 
-    for i in nums:
-        maximum = i if i > maximum else maximum
-
-    # Now keep applying it while k > 0
-    while k > 0:
-        nums = [maximum - ai for ai in nums]
-
-        # Subtract 1 here since it's one operation
-        k -= 1
-
-        # Find max again
-        maximum = nums[0]
-        
-        for i in nums:
-            maximum = i if i > maximum else maximum
-
-        
-    return ' '.join(map(str, nums))
 
 if __name__ == '__main__':
     t = int(input())

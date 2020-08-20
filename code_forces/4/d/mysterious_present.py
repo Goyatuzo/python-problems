8e3d1327ec765c55c480b1ff0eb657400f3dad6d
Yuto Otaguro <yotagur@gmail.com>
2020-08-20 16:03:38 -0500

    Attempt 1

--- code_forces/4/d/mysterious_present.py
+++ code_forces/4/d/mysterious_present.py
@@ -1,10 +1,32 @@
 from typing import List, Tuple
 
-def mysterious_present(w: int, h: int, cards: List[Tuple[int, int]]) -> Tuple[int, str]:
-    return (0, '')
+def mysterious_present(w: int, h: int, envelopes: List[Tuple[int, int]]) -> Tuple[int, str]:
+    # Attach original index to each envelope
+    envs = [(i + 1, dims) for i, dims in enumerate(envelopes)]
+    # Now sort the envelopes
+    envs = sorted(envs, key=lambda env: (env[1][0], env[1][1]))
+
+    chain = []
+    prev = (-1, -1)
+
+    for idx, dims in envs:
+        if prev == dims:
+            prev = dims
+            continue
+
+        chain.append(str(idx))
+        prev = dims
+
+    return (len(chain), ' '.join(chain))
 
 if __name__ == '__main__':
-    n = int(input())
+    n, w, h = map(int, input().split(' '))
 
+    envelopes = []
     for _ in range(n):
-        pass
+        env_w, env_h = map(int, input().split(' '))
+        envelopes.append((env_w, env_h))
+
+    res = mysterious_present(w, h, envelopes)
+    print(res[0])
+    print(res[1])

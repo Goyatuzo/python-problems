86582dc80a468de6db4762fae96846c0765d78de
Yuto Otaguro <yotagur@gmail.com>
2020-08-21 11:26:26 -0500

    First try

--- code_forces/1401/b/ternary_sequence.py
+++ code_forces/1401/b/ternary_sequence.py
@@ -3,7 +3,14 @@ def ternary_sequence(x0: int, y0: int, z0: int, x1: int, y1: int, z1: int) -> in
 	cum_sum = 0
 	total_runs = x0 + y0 + z0 + x1 + y1 + z1
 
-	pass
+	for _ in range(total_runs):
+		# Best cases
+		if z0 > 0 and y1 > 0:
+			z0 -= 1
+			y1 -= 1
+			cum_sum += 2
+
+	return cum_sum
 
 if __name__ == '__main__':
 	t = int(input())

0055ee10a1b9e29ddbad8c351f41e44d192e8fb9
Yuto Otaguro <yotagur@gmail.com>
2020-08-21 11:37:15 -0500

    Realize most can be thrown out

--- code_forces/1401/b/ternary_sequence.py
+++ code_forces/1401/b/ternary_sequence.py
@@ -1,16 +1,21 @@
 
 def ternary_sequence(x0: int, y0: int, z0: int, x1: int, y1: int, z1: int) -> int:
 	cum_sum = 0
-	total_runs = x0 + y0 + z0 + x1 + y1 + z1
 
-	for _ in range(total_runs):
-		# Best cases
-		if z0 > 0 and y1 > 0:
-			z0 -= 1
-			y1 -= 1
-			cum_sum += 2
+	two = min(z0, y1)
+	cum_sum = two * 2
+	z0 = z0 - two
+	y1 = y1 - two
 
-	return cum_sum
+	elim_twos = min(x0, z1)
+	z1 -= elim_twos
+	x0 -= elim_twos
+
+	elim_twos = min(z0, z1)
+	z1 -= elim_twos
+	z0 -= elim_twos
+
+	return cum_sum + (-2 * z1)
 
 if __name__ == '__main__':
 	t = int(input())

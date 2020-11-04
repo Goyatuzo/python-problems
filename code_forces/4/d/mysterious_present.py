6189310b4bc6a1063bbc0c9fb19d4055c720976f
Goyatuzo <yotagur@gmail.com>
2020-11-04 11:03:54 -0600

    Better mysterious presents

--- code_forces/4/d/mysterious_present.py
+++ code_forces/4/d/mysterious_present.py
@@ -1,21 +1,22 @@
 from typing import List, Tuple
 
+
 def mysterious_present(w: int, h: int, envelopes: List[Tuple[int, int]]) -> Tuple[int, str]:
     # Attach original index to each envelope, and filter out non-conformers
-    envs = [(i + 1, dims) for i, dims in enumerate(envelopes) if dims[0] > w and dims[1] > h]
+    envs = [(i + 1, dims)
+            for i, dims in enumerate(envelopes) if dims[0] > w and dims[1] > h]
     # Now sort the envelopes
     envs = sorted(envs, key=lambda env: (env[1][0], env[1][1]))
-    
-    # Store the chains in here
-    chain = [(0, el) for el in envs]
-    # Set first element in chain to 1
-    chain[0] = (1, chain[0][1])
-    
-    for i in range(len(envs)):
-        for j in range(i, len(envs)):
-            
 
+    chain = []
+    curr_dim = (w, h)
 
+    for env in envs:
+        if curr_dim[0] < env[1][0] and curr_dim[1] < env[1][1]:
+            chain.append(env[0])
+            curr_dim = env[1]
+        
+    return len(chain), ' '.join(map(str, chain))
 
 if __name__ == '__main__':
     n, w, h = map(int, input().split(' '))

99362d0349f1bccd6e897085a1df97ea11a47a95
Yuto Otaguro <yotagur@gmail.com>
2020-11-25 14:27:50 -0600

    Try to fix one problem

--- code_forces/4/d/mysterious_present.py
+++ code_forces/4/d/mysterious_present.py
@@ -5,13 +5,16 @@ def mysterious_present(w: int, h: int, envelopes: List[Tuple[int, int]]) -> Tupl
     # Attach original index to each envelope, and filter out non-conformers
     envs = [(i + 1, dims)
             for i, dims in enumerate(envelopes) if dims[0] > w and dims[1] > h]
-    # Now sort the envelopes
-    envs = sorted(envs, key=lambda env: (env[1][0], env[1][1]))
 
-    chain = []
+    # Now sort the envelopes
+    xenvs = sorted(envs, key=lambda env: (env[1][0], env[1][1]))
+    yenvs = sorted(envs, key=lambda env: (env[1][0], env[1][1]))
+
+    chain = [[None] * len(envs)]] * len(envs)
     curr_dim = (w, h)
 
-    for env in envs:
+
+    for env in xenvs:
         if curr_dim[0] < env[1][0] and curr_dim[1] < env[1][1]:
             chain.append(env[0])
             curr_dim = env[1]

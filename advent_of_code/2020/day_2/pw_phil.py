c8e036cbeb41eb149ff187da411b3a29dd3d64c5
Goyatuzo <yotagur@gmail.com>
2020-12-02 00:00:48 -0600

    Complete part 1

--- advent_of_code/2020/day_2/pw_phil.py
+++ advent_of_code/2020/day_2/pw_phil.py
@@ -1,3 +1,29 @@
 
 def pw_phil_valid(line: str) -> bool:
-    pass
+    # Split into three tokens by space
+    counts, letter, pw = line.split(' ')
+
+    # Get min and max of the letter
+    minimum, maximum = [int(v) for v in counts.split('-')]
+
+    # Get the letter to be evaluated
+    letter = letter[0]
+
+    occurance = pw.count(letter)
+
+    return occurance >= minimum and occurance <= maximum
+
+if __name__ == "__main__":
+    lst = []
+    valid_count = 0
+
+    with open('inputs.txt', 'r') as f:
+        line = f.readline()
+
+        while line:
+            if pw_phil_valid(line):
+                valid_count += 1
+
+            line = f.readline()
+
+    print(f'Part 1: {valid_count}')

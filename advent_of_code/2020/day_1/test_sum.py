2cf000026b0a5f2658c709b33eda1405d910f4d5
Goyatuzo <yotagur@gmail.com>
2020-12-01 19:33:08 -0600

    UT for part 2

--- advent_of_code/2020/day_1/test_sum.py
+++ advent_of_code/2020/day_1/test_sum.py
@@ -1,10 +1,18 @@
 import unittest
 
-from sum import sum_aoc_sort
+from sum import sum_aoc_sort, sum_three
 
 class TestSumAOC(unittest.TestCase):
     def test_given(self):
         self.assertEqual(514579, sum_aoc_sort([1721,
+        979,
+        366,
+        299,
+        675,
+        1456], 2020))
+
+    def test_given_two(self):
+        self.assertEqual(241861950, sum_three([1721,
         979,
         366,
         299,

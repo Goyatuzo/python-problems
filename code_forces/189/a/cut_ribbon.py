184dacb786933d420153e051ccb12656fab785f1
Goyatuzo <yotagur@gmail.com>
2020-12-02 03:08:36 -0600

    Accepted answer

--- code_forces/189/a/cut_ribbon.py
+++ code_forces/189/a/cut_ribbon.py
@@ -1,30 +1,22 @@
 def cut_ribbon(n: int, a: int, b: int, c: int) -> int:
-    # Create list of n length 
     cache = [0] * (n + a + b + c)
 
-    # Initialize first values to 1
-    for i in range(int(n / a)):
-        idx = i * a
-        cache[idx] = i
-        cache[idx + b] = i + 1
-        cache[idx + c] = i + 1
-        cache[idx + b + c] = i + 2
+    # Set the initial states to 1
+    cache[a] = 1
+    cache[b] = 1
+    cache[c] = 1
 
-    # Now for the second values
-    for i in range(int(n / b)):
-        idx = i * b
-        cache[idx] = max(cache[idx], i)
-        cache[idx + a] = max(cache[idx + a], i + 1)
-        cache[idx + c] = max(cache[idx + c], i + 1)
-        cache[idx + a + c] = max(cache[idx + a + c], i + 2)
-
-
-    for i in range(int(n / c)):
-        idx = i * c
-        cache[idx] = max(cache[idx], i)
-        cache[idx + a] = max(cache[idx + a], i + 1)
-        cache[idx + b] = max(cache[idx + b], i + 1)
-        cache[idx + a + b] = max(cache[idx + a + b], i + 2)
-
+    for i in range(n):
+        # If the stored value is 0, then it's not part of the sequence
+        if cache[i] != 0:
+            cache[i + a] = max(cache[i] + 1, cache[i + a])
+            cache[i + b] = max(cache[i] + 1, cache[i + b])
+            cache[i + c] = max(cache[i] + 1, cache[i + c])
 
     return cache[n]
+
+
+if __name__ == '__main__':
+    n, a, b, c = list(map(int, input().split(' ')))
+
+    print(cut_ribbon(n, a, b, c))
--- code_forces/189/a/test_cut_ribbon.py
+++ code_forces/189/a/test_cut_ribbon.py
@@ -12,6 +12,18 @@ class TestCutRibbon(unittest.TestCase):
         """The only possible cut is for a single 5-length"""
         self.assertEqual(1, cut_ribbon(5, 5, 4, 2))
 
+    def test_code_forces(self):
+        self.assertEqual(11, cut_ribbon(3119, 3515, 1021, 7))
+        self.assertEqual(1, cut_ribbon(1, 1, 1, 1))
+        self.assertEqual(4000, (cut_ribbon(4000, 1, 2, 3)))
+
+    def test_ones(self):
+        self.assertEqual(10, cut_ribbon(10, 1, 2, 3))
+        self.assertEqual(2, cut_ribbon(2, 1, 1, 1))
+        self.assertEqual(5, cut_ribbon(10, 50, 2, 30))
+        self.assertEqual(5, cut_ribbon(11, 2, 3, 5))
+
+
 
 
 if __name__ == '__main__':

f817a55a3ed6980ff391ff44c41dccb8d0039e85
Goyatuzo <yotagur@gmail.com>
2020-12-04 01:53:56 -0600

    Tests pass

--- advent_of_code/2020/day_3/toboggan.py
+++ advent_of_code/2020/day_3/toboggan.py
@@ -1,4 +1,21 @@
 from typing import List
 
-def tobaggan_one(l: List[int]):
-    return 0
+def str_to_lst(og: str) -> List[str]:
+    return og.replace(' ', '').split('\n') 
+
+def tobaggan_one(l: str) -> int:
+    lst = str_to_lst(l)
+
+    tree = 0
+    i = 0
+    width = len(lst[0])
+    for y in range(1, len(lst)):
+        # Three to the right, wrap around if over max
+        i = (i + 3) % width
+
+
+        if lst[y][i] == '#':
+            tree += 1
+
+
+    return tree

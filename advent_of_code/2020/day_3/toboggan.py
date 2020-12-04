86579a30b2586453327107822c8d0a97ac90798f
Goyatuzo <yotagur@gmail.com>
2020-12-04 16:41:59 -0600

    Initial condition for down 2 was incorrect

--- advent_of_code/2020/day_3/toboggan.py
+++ advent_of_code/2020/day_3/toboggan.py
@@ -6,7 +6,7 @@ def tobaggan_gen(lst: List[str], step: int, down: int) -> int:
     tree = 0
     i = 0
     width = len(lst[0])
-    for y in range(1, len(lst), down):
+    for y in range(down, len(lst), down):
         # Three to the right, wrap around if over max
         i = (i + step) % width
 

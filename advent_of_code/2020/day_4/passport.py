1ed765408532953ab3712cb8de23e3b7bb928928
Goyatuzo <yotagur@gmail.com>
2020-12-04 17:19:03 -0600

    Complete part 1

--- advent_of_code/2020/day_4/passport.py
+++ advent_of_code/2020/day_4/passport.py
@@ -1,16 +1,48 @@
 from typing import List
 from pathlib import Path
-from os import path
+from os import MFD_HUGETLB, path
 
 def get_file(fname: str) -> List[str]:
     with open(path.join(Path(__file__).parent, fname), 'r') as f:
-        lines = f.readline()
+        lines = f.readlines()
 
         # Strip out the newlines since it was messing up the number
         return [line.strip() for line in lines]
 
 def passport_one(fname: str):
-    pass
+    lst = get_file(fname)
+
+    curr_pass = ''
+    valid_passports = 0
+    for line in lst:
+        if line == '':
+            # Get all the current variables
+            entries = sorted(curr_pass.split(' '))
+
+            required = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+            # BYR
+            for entry in entries:                    
+                field = entry.split(':')
+
+                try:
+                    required.remove(field[0])
+                except:
+                    pass
+
+            if len(required) == 0:
+                valid_passports += 1
+
+            # Reset the accumulator variable
+            curr_pass = ''
+        else:
+            # Add empty space to help with splitting
+            curr_pass += line + ' '
+
+    return valid_passports
 
 def passport_two(fname: str):
-    pass
+    return 0
+
+if __name__ == "__main__":
+    print(f'Part 1: {passport_one("input.txt")}')
+    print(f'Part 2: {passport_two("input.txt")}')

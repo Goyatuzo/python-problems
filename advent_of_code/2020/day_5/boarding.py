61c8a9e0255f434facedd15b3f103e74e8878cde
Yuto Otaguro <yotagur@gmail.com>
2020-12-24 12:49:04 -0600

    boarding_ticket returns a value

--- advent_of_code/2020/day_5/boarding.py
+++ advent_of_code/2020/day_5/boarding.py
@@ -1,14 +1,12 @@
-from typing import List
-from pathlib import Path
-from os import path
+from math import ceil
 
 def boarding_ticket(ticket: str) -> int:
     row = (0, 128)
     column = (0, 7)
 
     for char in ticket:
-        row_avg = (row[0] + row[1]) / 2
-        column_avg = (column[0] + column[1]) / 2
+        row_avg = ceil((row[0] + row[1]) / 2)
+        column_avg = ceil((column[0] + column[1]) / 2)
         if char == 'F':
             row = (row_avg, row[1])
         elif char == 'B':
@@ -16,10 +14,10 @@ def boarding_ticket(ticket: str) -> int:
         elif char == 'L':
             column = (column[0], column_avg)
         elif char == 'R':
-            column = (column[1] / 2, column[1])
+            column = (column_avg, column[1])
 
+    # Look at the last row char to find the row number
+    row = row[0] if ticket[6] == 'F' else row[1]
+    column = column[0] if ticket[-1] == 'L' else column[1]
 
-
-if __name__ == "__main__":
-    print(f'Part 1: {tobaggan_one("input.txt")}')
-    print(f'Part 2: {tobaggan_two("input.txt")}')
+    return row * column

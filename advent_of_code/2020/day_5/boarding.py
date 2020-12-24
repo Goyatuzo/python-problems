d5126b1c6d22ebfa410b8b4af671630a7f0d9a86
Yuto Otaguro <yotagur@gmail.com>
2020-12-24 13:29:31 -0600

    Fix the boundaries, and alter floor / ceil operations

--- advent_of_code/2020/day_5/boarding.py
+++ advent_of_code/2020/day_5/boarding.py
@@ -1,21 +1,36 @@
+from math import floor, ceil
+
 def boarding_ticket(ticket: str) -> int:
-    row = (0, 128)
+    row = (0, 127)
     column = (0, 7)
 
     for char in ticket:
-        row_avg = int((row[0] + row[1]) / 2)
-        column_avg = int((column[0] + column[1]) / 2)
+        row_avg = (row[0] + row[1]) / 2
+        column_avg = (column[0] + column[1]) / 2
         if char == 'F':
-            row = (row[0], row_avg)
+            row = (row[0], floor(row_avg))
         elif char == 'B':
-            row = (row_avg, row[1])
+            row = (ceil(row_avg), row[1])
         elif char == 'L':
-            column = (column[0], column_avg)
+            column = (column[0], floor(column_avg))
         elif char == 'R':
-            column = (column_avg, column[1])
+            column = (ceil(column_avg), column[1])
 
     # Look at the last row char to find the row number
     row = row[0] if ticket[6] == 'F' else row[1]
     column = column[0] if ticket[-1] == 'L' else column[1]
 
     return row * 8 + column
+
+if __name__ == "__main__":
+    with open('inputs.txt', 'r') as f:
+        line = f.readline().strip()
+
+        max_seat = 0
+
+        while line:
+            max_seat = max(max_seat, boarding_ticket(line))
+            line = f.readline().strip()
+
+
+    print(f'Part 1: {max_seat}')

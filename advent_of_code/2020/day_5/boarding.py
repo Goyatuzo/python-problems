f16a6885dc729b589b25f6ce546ebb8d3b776f41
Yuto Otaguro <yotagur@gmail.com>
2020-12-24 13:03:34 -0600

    Fix eq and need to take floor, not ceil

--- advent_of_code/2020/day_5/boarding.py
+++ advent_of_code/2020/day_5/boarding.py
@@ -1,16 +1,14 @@
-from math import ceil
-
 def boarding_ticket(ticket: str) -> int:
     row = (0, 128)
     column = (0, 7)
 
     for char in ticket:
-        row_avg = ceil((row[0] + row[1]) / 2)
-        column_avg = ceil((column[0] + column[1]) / 2)
+        row_avg = int((row[0] + row[1]) / 2)
+        column_avg = int((column[0] + column[1]) / 2)
         if char == 'F':
-            row = (row_avg, row[1])
-        elif char == 'B':
             row = (row[0], row_avg)
+        elif char == 'B':
+            row = (row_avg, row[1])
         elif char == 'L':
             column = (column[0], column_avg)
         elif char == 'R':
@@ -20,4 +18,4 @@ def boarding_ticket(ticket: str) -> int:
     row = row[0] if ticket[6] == 'F' else row[1]
     column = column[0] if ticket[-1] == 'L' else column[1]
 
-    return row * column
+    return row * 8 + column

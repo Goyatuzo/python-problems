c8622afdc0a670e66bf6d2953ecf4476c22bff50
Yuto Otaguro <yotagur@gmail.com>
2020-12-25 14:26:22 -0600

    Finish part 1

--- advent_of_code/2020/day_6/customs.py
+++ advent_of_code/2020/day_6/customs.py
@@ -1,15 +1,51 @@
-from math import floor, ceil
+from typing import List
 
 class Customs:
-	answered_yes: int
+	__group_answers: List[List[str]]
+	__current_group: List[str]
+
 	def __init__(self):
-		self.__answered_yes = 0
+		self.__group_answers = []
+		self.__current_group = []
+
 
 	def process_answers(self, answers: str):
-		self.__answered_yes += len(answers)
+		# If a new group is being processed, add to list of groups and start a new one
+		if answers == '':
+			self.__group_answers.append(self.__current_group)
+			self.__current_group = []
+			return
+
+		# Otherwise, add the response to the current group
+		self.__current_group.append(answers)
 
 	def part_one(self) -> int:
-		return self.__answered_yes
+		# When it first runs, the last group doesn't get added
+		self.__group_answers.append(self.__current_group)
+		self.__current_group = []
+
+		unique_answers = 0
+
+		for group in self.__group_answers:
+			concated_answers = ''.join(group)
+			# Use sorting to get worst case n lg n, avoid dict
+			sorted_answers = sorted(concated_answers)
+
+			group_count = 0
+			for i, char in enumerate(sorted_answers):
+				# We need a previous character
+				if i == 0:
+					group_count = 1
+					continue
+
+				# If the prev char and curr are the same, it's a duplicate
+				if sorted_answers[i - 1] != char:
+					group_count += 1
+
+			unique_answers += group_count
+
+
+		return unique_answers
 
 if __name__ == '__main__':
 	customs = Customs()

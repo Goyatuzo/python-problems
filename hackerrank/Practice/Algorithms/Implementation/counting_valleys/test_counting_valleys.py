295e5355720abccc3e5ce6726772ac87f17c8ce6
Goyatuzo <yotagur@gmail.com>
2022-08-01 00:08:34 -0500

    Add the problems

---
+++ hackerrank/Practice/Algorithms/Implementation/counting_valleys/test_counting_valleys.py
@@ -0,0 +1,12 @@
+import unittest
+
+from counting_valleys import counting_valleys
+
+class TestCountingValleys(unittest.TestCase):
+    def test_problem(self):
+        self.assertEqual(1, counting_valleys(8, "UDDDUDUU"))
+
+
+if __name__ == '__main__':
+    unittest.main()
+

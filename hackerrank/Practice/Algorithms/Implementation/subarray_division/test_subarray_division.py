4673fc1a118dc38b6ea76935b55a44f23cd32c73
Goyatuzo <yotagur@gmail.com>
2022-08-17 00:31:52 -0500

    Write other two tests

--- hackerrank/Practice/Algorithms/Implementation/subarray_division/test_subarray_division.py
+++ hackerrank/Practice/Algorithms/Implementation/subarray_division/test_subarray_division.py
@@ -9,6 +9,12 @@ class TestSubarrayDivision(unittest.TestCase):
     def test_problem(self):
         self.assertEqual(2, subarray_division([1, 2, 1, 3, 2], 3, 2))
 
+    def test_second(self):
+        self.assertEqual(0, subarray_division([1, 1, 1, 1, 1, 1], 3, 2))
+
+    def test_three(self):
+        self.assertEqual(1, subarray_division([4], 4, 1))
+
 if __name__ == '__main__':
     unittest.main()
 

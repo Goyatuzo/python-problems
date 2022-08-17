4c8abac2df3a8d5b359b9484efdad2158df0d151
Goyatuzo <yotagur@gmail.com>
2022-08-17 00:25:10 -0500

    Add example test

--- hackerrank/Practice/Algorithms/Implementation/subarray_division/test_subarray_division.py
+++ hackerrank/Practice/Algorithms/Implementation/subarray_division/test_subarray_division.py
@@ -3,6 +3,9 @@ import unittest
 from subarray_division import subarray_division 
 
 class TestSubarrayDivision(unittest.TestCase):
+    def test_given(self):
+        self.assertEqual(2, subarray_division([2, 2, 1, 3, 2], 4, 2))
+
     def test_problem(self):
         self.assertEqual(2, subarray_division([1, 2, 1, 3, 2], 3, 2))
 

5ec322eec7d520b464f2e62734e60f6b207db011
Yuto Otaguro <yotagur@gmail.com>
2022-08-19 05:47:14 +0000

    Lc contains duplicate (#49)
    * initial commit
    
    * Forgot leetcode cares about naming
    
    * Initial attempt, use set
    
    * Update file names to not clash
    
    * Forgot to update import name

---
+++ leetcode/217/test_counting_duplicate.py
@@ -0,0 +1,22 @@
+import unittest
+
+from counting_duplicate import Solution
+
+
+class TestContainsDuplicate(unittest.TestCase):
+    def test_1(self):
+        s = Solution()
+        self.assertEqual(True, s.containsDuplicate([1, 2, 3, 1]))
+
+    def test_2(self):
+        s = Solution()
+        self.assertEqual(False, s.containsDuplicate([1, 2, 3, 4]))
+
+    def test_3(self):
+        s = Solution()
+        self.assertEqual(True, s.containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4,
+                                                    2]))
+
+
+if __name__ == '__main__':
+    unittest.main()

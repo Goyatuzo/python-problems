697493db7745caa8037be55770239e1818a66836
Goyatuzo <yotagur@gmail.com>
2022-08-19 00:02:25 -0500

    Initial commit

---
+++ hackerrank/Practice/Algorithms/Implementation/day_of_the_programmer/day_of_the_programmer.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'dayOfProgrammer' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts INTEGER year as parameter.
+#
+
+def day_of_the_programmer(year):
+    pass
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    year = int(input().strip())
+
+    result = dayOfProgrammer(year)
+
+    fptr.write(result + '\n')
+
+    fptr.close()

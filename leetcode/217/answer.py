fd02122ddd256f1982eb9628505c3d0e516a1a27
Goyatuzo <yotagur@gmail.com>
2022-08-19 00:40:03 -0500

    Initial attempt, use set

--- leetcode/217/answer.py
+++ leetcode/217/answer.py
@@ -1,5 +1,12 @@
 from typing import List
 
 class Solution:
-    def containsDuplicate(self, s: List[str]) -> bool:
-        pass
+    def containsDuplicate(self, s: List[int]) -> bool:
+        count = set()
+
+        for val in s:
+            if val in count:
+                return True
+            count.add(val)
+
+        return False

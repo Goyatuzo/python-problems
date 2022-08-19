2acbac916f48f90d48cd0d88774baa8159ce7a18
Goyatuzo <yotagur@gmail.com>
2022-08-19 00:12:47 -0500

    Initial implementation

--- hackerrank/Practice/Algorithms/Implementation/day_of_the_programmer/day_of_the_programmer.py
+++ hackerrank/Practice/Algorithms/Implementation/day_of_the_programmer/day_of_the_programmer.py
@@ -14,14 +14,26 @@ import sys
 #
 
 def day_of_the_programmer(year):
-    pass
+    is_gregorian_leap_year = year % 400 == 0 or (year % 4 == 0 and year % 100
+                                                 != 0)
+    is_julian_leap_year = year % 4 == 0
+    
+
+    if year > 1918:
+        if is_gregorian_leap_year:
+            return f"12.09.{year}"
+        return f"13.09.{year}"
+    else:
+        if is_julian_leap_year:
+            return f"12.09.{year}"
+        return f"13.09.{year}"
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     year = int(input().strip())
 
-    result = dayOfProgrammer(year)
+    result = day_of_the_programmer(year)
 
     fptr.write(result + '\n')
 

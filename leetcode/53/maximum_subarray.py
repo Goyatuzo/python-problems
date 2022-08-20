7c740379e40a9914df62d91b4ca786131f7002fd
Goyatuzo <yotagur@gmail.com>
2022-08-20 00:54:26 -0500

    Completed the question

--- leetcode/53/maximum_subarray.py
+++ leetcode/53/maximum_subarray.py
@@ -2,4 +2,10 @@ from typing import List
 
 class Solution:
     def maxSubArray(self, s: List[int]) -> bool:
-        pass
+        maximums = [s[0]]
+
+        for num in s[1:]:
+            curr_max = max(maximums[-1] + num, num)
+            maximums.append(curr_max)
+
+        return max(maximums)
--- leetcode/53/test_maximum_subarray.py
+++ leetcode/53/test_maximum_subarray.py
@@ -16,6 +16,10 @@ class TestMaximumSubarray(unittest.TestCase):
         s = Solution()
         self.assertEqual(23, s.maxSubArray([5,4,-1,7,8]))
 
+    def test_4(self):
+        s = Solution()
+        self.assertEqual(-1, s.maxSubArray([-1, -1, -1]))
+
 
 if __name__ == '__main__':
     unittest.main()

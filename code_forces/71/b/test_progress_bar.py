10fdfee0ad49745f5c126fb7f5e8bc3339b4fec9
Goyatuzo <yotagur@gmail.com>
2023-01-07 00:48:19 -0600

    Update tests

--- code_forces/71/b/test_progress_bar.py
+++ code_forces/71/b/test_progress_bar.py
@@ -4,10 +4,14 @@ from progress_bar import progress_bar as foo
 
 class TestProgressBar(unittest.TestCase):
     def test_given_1(self):
-        self.assertEqual([10, 10, 10, 10, 10, 4, 0, 0, 0, 0], foo([10, 10, 54]))
+        self.assertEqual(['10', '10', '10', '10', '10', '4', '0', '0', '0', '0'], foo([10, 10, 54]))
 
     def test_given_2(self):
-        self.assertEqual([13, 13, 13, 13, 0, 0, 0, 0, 0, 0, 0], foo([11, 13, 37]))
+        self.assertEqual(['13', '13', '13', '13', '0', '0', '0', '0', '0', '0', '0'], foo([11, 13, 37]))
+
+    def test_8(self):
+        self.assertEqual(['13' for _ in range(17)], foo([17, 13, 100]))
+
 
 if __name__ == '__main__':
     unittest.main()

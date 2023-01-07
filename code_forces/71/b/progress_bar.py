0688f999ebc22f48ab76657a96a10440303c18d1
Goyatuzo <yotagur@gmail.com>
2023-01-07 00:28:35 -0600

    First test passes

--- code_forces/71/b/progress_bar.py
+++ code_forces/71/b/progress_bar.py
@@ -1,8 +1,22 @@
-def progress_bar(word: str) -> str:
-	pass
+from typing import List
+
+def progress_bar(nums: List[int]) -> str:
+	[n, k, t] = nums
+	bar = [0 for _ in range(n)]
+
+	for i in range(n):
+		print(t, k, t - k)
+		if t > k:
+			bar[i] = k
+		elif t > 0:
+			bar[i] = t
+		else:
+			bar[i] = 0
+		t = max(0, t - k)
+
+	return bar
+
 
 if __name__ == '__main__':
-    n = int(input())
-
-    for _ in range(n):
-        print(progress_bar([int(num) for num in input().split()]))
+	for _ in range(n):
+		print(progress_bar([int(num) for num in input().split()]))
--- code_forces/71/b/test_progress_bar.py
+++ code_forces/71/b/test_progress_bar.py
@@ -2,9 +2,11 @@ import unittest
 
 from progress_bar import progress_bar as foo
 
-class TestWayTooLongWords(unittest.TestCase):
-    def test_given(self):
+class TestProgressBar(unittest.TestCase):
+    def test_given_1(self):
         self.assertEqual([10, 10, 10, 10, 10, 4, 0, 0, 0, 0], foo([10, 10, 54]))
+
+    def test_given_2(self):
         self.assertEqual([13, 13, 13, 13, 0, 0, 0, 0, 0, 0, 0], foo([11, 13, 37]))
 
 if __name__ == '__main__':

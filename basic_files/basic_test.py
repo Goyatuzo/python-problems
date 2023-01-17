f17c2a9dbfb62055690da39a927ff5e7f099b841
Goyatuzo <yotagur@gmail.com>
2023-01-16 23:33:53 -0600

    Create basic files

---
+++ basic_files/basic_test.py
@@ -0,0 +1,14 @@
+import unittest
+
+from basic import basic as foo
+
+class Testbasic(unittest.TestCase):
+	def test_given_1(self):
+		pass
+
+	def test_given_2(self):
+		pass
+
+
+if __name__ == '__main__':
+    unittest.main()

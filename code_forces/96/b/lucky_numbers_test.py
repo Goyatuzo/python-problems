c57574518bccc49e6c469a7980bd1925b8076ed9
Goyatuzo <yotagur@gmail.com>
2023-01-17 00:06:31 -0600

    Update tests

--- code_forces/96/b/lucky_numbers_test.py
+++ code_forces/96/b/lucky_numbers_test.py
@@ -4,10 +4,10 @@ from lucky_numbers import lucky_numbers as foo
 
 class TestLuckyNumbers(unittest.TestCase):
 	def test_given_1(self):
-		pass
+		self.assertEqual(foo('4500', '4747'))	
 
 	def test_given_2(self):
-		pass
+		self.assertEqual(foo('47', '47'))	
 
 
 if __name__ == '__main__':

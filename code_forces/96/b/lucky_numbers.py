ecb47d7894aa3ccc6ca44822869badf3a060df0d
Goyatuzo <yotagur@gmail.com>
2023-01-17 00:05:43 -0600

    Create files

---
+++ code_forces/96/b/lucky_numbers.py
@@ -0,0 +1,5 @@
+def lucky_numbers():
+	pass
+
+if __name__ == '__main__':
+	print(lucky_numbers())
---
+++ code_forces/96/b/lucky_numbers_test.py
@@ -0,0 +1,14 @@
+import unittest
+
+from lucky_numbers import lucky_numbers as foo
+
+class TestLuckyNumbers(unittest.TestCase):
+	def test_given_1(self):
+		pass
+
+	def test_given_2(self):
+		pass
+
+
+if __name__ == '__main__':
+    unittest.main()

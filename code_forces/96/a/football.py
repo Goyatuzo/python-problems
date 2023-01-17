ae1149cb4d88ca59514f9d931c52aff30f73934b
Goyatuzo <yotagur@gmail.com>
2023-01-16 23:52:18 -0600

    Create basic files

---
+++ code_forces/96/a/football.py
@@ -0,0 +1,5 @@
+def football():
+	pass
+
+if __name__ == '__main__':
+	print(football())
---
+++ code_forces/96/a/football_test.py
@@ -0,0 +1,14 @@
+import unittest
+
+from football import football as foo
+
+class TestFootball(unittest.TestCase):
+	def test_given_1(self):
+		pass
+
+	def test_given_2(self):
+		pass
+
+
+if __name__ == '__main__':
+    unittest.main()

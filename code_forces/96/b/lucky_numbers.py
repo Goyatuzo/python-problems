681ab614dc864a8a132f0eeb52ea0bb3eaadd6a5
Goyatuzo <yotagur@gmail.com>
2023-01-19 00:22:21 -0600

    Implement creating all super lucky numbers

--- code_forces/96/b/lucky_numbers.py
+++ code_forces/96/b/lucky_numbers.py
@@ -1,5 +1,39 @@
-def lucky_numbers():
-	pass
+from collections import deque
+from typing import List
+
+def is_super_lucky(number: str) -> bool:
+	fours = 0
+	sevens = 0
+
+	for digit in number:
+		if digit == '4':
+			fours += 1
+		elif digit == '7':
+			sevens += 1
+			
+	return fours == sevens and sevens > 0
+
+def generate_super_lucky_numbers(n: int) -> List[int]:
+	numbers = []
+	queue = deque([''])
+
+	while queue:
+		candidate = queue.pop()
+
+		if len(candidate) <= n:
+			queue.appendleft(candidate + '4')
+			queue.appendleft(candidate + '7')
+
+		if is_super_lucky(candidate):
+			numbers.append(candidate)
+
+
+	return numbers
+
+def lucky_numbers(number: str) -> str:
+	luckies = generate_super_lucky_numbers(9)
+
+	return ''
 
 if __name__ == '__main__':
 	print(lucky_numbers())
--- code_forces/96/b/lucky_numbers_test.py
+++ code_forces/96/b/lucky_numbers_test.py
@@ -4,10 +4,14 @@ from lucky_numbers import lucky_numbers as foo
 
 class TestLuckyNumbers(unittest.TestCase):
 	def test_given_1(self):
-		self.assertEqual(foo('4500', '4747'))	
+		self.assertEqual(foo('4500'), '4747')	
 
 	def test_given_2(self):
-		self.assertEqual(foo('47', '47'))	
+		self.assertEqual(foo('47'), '47')	
+
+	def test_edge_1(self):
+		self.assertEqual(foo('4900'), '7474')
+
 
 
 if __name__ == '__main__':

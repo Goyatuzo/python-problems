77e7a8042497f733903fa9848af8f0bf5909adb2
Goyatuzo <yotagur@gmail.com>
2023-01-20 00:11:57 -0600

    Create files

---
+++ code_forces/25/a/iq_test.py
@@ -0,0 +1,5 @@
+def iq_test():
+	pass
+
+if __name__ == '__main__':
+	print(iq_test())
---
+++ code_forces/25/a/iq_test_test.py
@@ -0,0 +1,14 @@
+import unittest
+
+from iq_test import iq_test as foo
+
+class Testiq_test(unittest.TestCase):
+	def test_given_1(self):
+		pass
+
+	def test_given_2(self):
+		pass
+
+
+if __name__ == '__main__':
+    unittest.main()

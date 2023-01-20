55a367edd5768482ee58ef185b571a09dd2f823c
Goyatuzo <yotagur@gmail.com>
2023-01-20 00:35:17 -0600

    Complete

--- code_forces/25/a/iq_test.py
+++ code_forces/25/a/iq_test.py
@@ -1,5 +1,26 @@
-def iq_test():
-	pass
+from typing import List
+
+def iq_test(numbers: List[int]) -> int:
+	evens = 0
+	odds = 0
+
+	first_even = -1
+	first_odd = -1
+
+	for i, num in enumerate(numbers):
+		if num % 2 == 0:
+			evens += 1
+			
+			if first_even < 0:
+				first_even = i
+		else:
+			odds += 1
+
+			if first_odd < 0:
+				first_odd = i
+
+	return first_even + 1 if evens == 1 else first_odd + 1
 
 if __name__ == '__main__':
-	print(iq_test())
+	_ = input()
+	print(iq_test([int(i) for i in input().split(' ')]))
--- code_forces/25/a/iq_test_test.py
+++ code_forces/25/a/iq_test_test.py
@@ -4,10 +4,10 @@ from iq_test import iq_test as foo
 
 class Testiq_test(unittest.TestCase):
 	def test_given_1(self):
-		pass
+		self.assertEqual(foo([2, 4, 7, 8, 10]), 3)
 
 	def test_given_2(self):
-		pass
+		self.assertEqual(foo([1, 2, 1, 1]), 2)
 
 
 if __name__ == '__main__':

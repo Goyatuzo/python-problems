91f565f312837c15e603a0a68aba902caf46672f
Goyatuzo <yotagur@gmail.com>
2023-01-23 22:50:01 -0600

    Complete odd

--- code_forces/25/b/phone_numbers.py
+++ code_forces/25/b/phone_numbers.py
@@ -4,7 +4,12 @@ def phone_numbers(raw_number: str) -> str:
 	if n % 2 == 0:
 		return '-'.join([raw_number[i:i+2] for i in range(0, n, 2)])
 
-	return ''
+	# Odd case here
+	last_three = raw_number[-3:]
+	rest = raw_number[:-3]
+
+	return '-'.join([rest[i:i+2] for i in range(0, len(rest), 2)]) + f'-{last_three}'
 
 if __name__ == '__main__':
-	print(phone_numbers())
+	input()
+	print(phone_numbers(input()))

0c5e3f7072f7288743015af3e7e3184341acd500
Goyatuzo <yotagur@gmail.com>
2023-01-23 22:34:52 -0600

    Write the tests

--- code_forces/25/b/phone_numbers_test.py
+++ code_forces/25/b/phone_numbers_test.py
@@ -4,10 +4,10 @@ from phone_numbers import phone_numbers as foo
 
 class Testphone_numbers(unittest.TestCase):
 	def test_given_1(self):
-		pass
+		self.assertEqual(foo('549871'), '54-98-71')
 
 	def test_given_2(self):
-		pass
+		self.assertEqual(foo('1198733'), '11-987-33')
 
 
 if __name__ == '__main__':

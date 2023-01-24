e2407a72cdc83a6b68830fd7bf23ae2eafbd3c2b
Goyatuzo <yotagur@gmail.com>
2023-01-23 22:51:18 -0600

    Basic case

--- code_forces/25/b/phone_numbers.py
+++ code_forces/25/b/phone_numbers.py
@@ -1,6 +1,9 @@
 def phone_numbers(raw_number: str) -> str:
 	n = len(raw_number)
 
+	if n < 4:
+		return raw_number
+
 	if n % 2 == 0:
 		return '-'.join([raw_number[i:i+2] for i in range(0, n, 2)])
 

6760591c426fc109fd612c71c881c9ebd7d53ed3
Goyatuzo <yotagur@gmail.com>
2023-01-23 22:47:12 -0600

    Account for even case

--- code_forces/25/b/phone_numbers.py
+++ code_forces/25/b/phone_numbers.py
@@ -1,5 +1,10 @@
-def phone_numbers():
-	pass
+def phone_numbers(raw_number: str) -> str:
+	n = len(raw_number)
+
+	if n % 2 == 0:
+		return '-'.join([raw_number[i:i+2] for i in range(0, n, 2)])
+
+	return ''
 
 if __name__ == '__main__':
 	print(phone_numbers())
--- code_forces/25/b/phone_numbers_test.py
+++ code_forces/25/b/phone_numbers_test.py
@@ -7,7 +7,13 @@ class Testphone_numbers(unittest.TestCase):
 		self.assertEqual(foo('549871'), '54-98-71')
 
 	def test_given_2(self):
-		self.assertEqual(foo('1198733'), '11-987-33')
+		self.assertEqual(foo('1198733'), '11-98-733')
+
+	def test_edge_1(self):
+		self.assertEqual(foo('1234'), '12-34')
+
+	def test_edge_2(self):
+		self.assertEqual(foo('12345'), '12-345')
 
 
 if __name__ == '__main__':

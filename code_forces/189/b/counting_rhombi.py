027477fbc53c891622ca44f702bf9ae0291bb95a
Yuto Otaguro <yotagur@gmail.com>
2023-07-17 23:02:43 -0500

    Create files

---
+++ code_forces/189/b/counting_rhombi.py
@@ -0,0 +1,5 @@
+def counting_rhombi():
+	pass
+
+if __name__ == '__main__':
+	print(counting_rhombi())
---
+++ code_forces/189/b/counting_rhombi_test.py
@@ -0,0 +1,14 @@
+import unittest
+
+from counting_rhombi import counting_rhombi as foo
+
+class Test_Counting_rhombi(unittest.TestCase):
+	def test_given_1(self):
+		pass
+
+	def test_given_2(self):
+		pass
+
+
+if __name__ == '__main__':
+    unittest.main()

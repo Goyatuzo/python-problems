2d1684cd5cbc878d3e18f7c635e466d9856bc47a
Yuto Otaguro <yotagur@gmail.com>
2023-07-25 00:22:08 -0500

    Super inefficient

--- code_forces/189/b/counting_rhombi.py
+++ code_forces/189/b/counting_rhombi.py
@@ -1,5 +1,20 @@
-def counting_rhombi():
-	pass
+def counting_rhombi(w, h):
+    count = 0
+    larger = max(w, h)
+
+    for r in range(1, larger):
+        for x in range(w):
+            if x - r < 0 or x + r > w:
+                continue
+
+            for y in range(h):
+                if y - r < 0 or y + r > h:
+                    continue
+
+                count += 1
+    return count
+	
 
 if __name__ == '__main__':
-	print(counting_rhombi())
+    (a, b) = list(map(int, input().split(' ')))
+    print(counting_rhombi(a, b))
--- code_forces/189/b/counting_rhombi_test.py
+++ code_forces/189/b/counting_rhombi_test.py
@@ -3,11 +3,14 @@ import unittest
 from counting_rhombi import counting_rhombi as foo
 
 class Test_Counting_rhombi(unittest.TestCase):
-	def test_given_1(self):
-		pass
+    def test_given_1(self):
+        self.assertEqual(foo(2, 2), 1)
 
-	def test_given_2(self):
-		pass
+    def test_given_2(self):
+        self.assertEqual(foo(1, 2), 0)
+
+    def test_self(self):
+        self.assertEqual(foo(4, 4), 10)
 
 
 if __name__ == '__main__':

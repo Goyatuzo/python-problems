f11859837b53cf215c7149e9d1f738015fe00298
Yuto Otaguro <yotagur@gmail.com>
2023-08-05 02:34:02 -0500

    Lc 27 (#64)
    * Add files
    
    * Write tests
    
    * First attempt
    
    * Avg case is OK
    
    * Done
    
    * More efficient solution

---
+++ leetcode/27/remove_element_test.py
@@ -0,0 +1,24 @@
+import unittest
+
+from remove_element import remove_element as foo
+
+class Test_Remove_Element(unittest.TestCase):
+    def test_given_1(self):
+        t = [3, 2, 2, 3]
+        self.assertEqual(foo(t, 3), 2)
+        print(t)
+
+    def test_given_2(self):
+        self.assertEqual(foo([0, 1, 2, 2, 3, 0, 4, 2], 2), 5)
+
+    def test_self_1(self):
+        self.assertEqual(foo([2, 2, 2, 3], 2), 1)
+
+    def test_self_2(self):
+        self.assertEqual(foo([5], 2), 1)
+
+    def test_self_3(self):
+        self.assertEqual(foo([5], 5), 0)
+
+if __name__ == '__main__':
+    unittest.main()

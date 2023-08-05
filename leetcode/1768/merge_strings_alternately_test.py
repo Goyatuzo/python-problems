83722dd09c33b19b5280c17f01bc312938d6a0b6
Yuto Otaguro <yotagur@gmail.com>
2023-08-05 02:33:19 -0500

    Lc 1768 (#63)
    * Add files
    
    * Write test cases
    
    * Done

---
+++ leetcode/1768/merge_strings_alternately_test.py
@@ -0,0 +1,17 @@
+import unittest
+
+from merge_strings_alternately import merge_strings_alternately as foo
+
+class Test_Merge_Strings_Alternately(unittest.TestCase):
+    def test_given_1(self):
+        self.assertEqual(foo("abc", "pqr"), "apbqcr")
+
+    def test_given_2(self):
+        self.assertEqual(foo("ab", "pqrs"), "apbqrs")
+
+    def test_given_3(self):
+        self.assertEqual(foo("abcd", "pq"), "apbqcd")
+
+
+if __name__ == '__main__':
+    unittest.main()

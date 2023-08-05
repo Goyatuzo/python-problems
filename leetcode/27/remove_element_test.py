e51a269d1748e462781ef40794653573ad596717
Yuto Otaguro <yotagur@gmail.com>
2023-08-05 01:53:42 -0500

    Write tests

--- leetcode/27/remove_element_test.py
+++ leetcode/27/remove_element_test.py
@@ -2,12 +2,12 @@ import unittest
 
 from remove_element import remove_element as foo
 
-class Testremove_element(unittest.TestCase):
+class Test_Remove_Element(unittest.TestCase):
 	def test_given_1(self):
-		pass
+        self.assertEqual(foo([2, 2, 3, 3], 3), 2)
 
 	def test_given_2(self):
-		pass
+        self.assertEqual(foo([0, 1, 4, 0, 3, 2, 2, 2], 2), 5)
 
 
 if __name__ == '__main__':

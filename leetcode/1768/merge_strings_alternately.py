759d1944d204be0890050018de64adff0bc9d2e0
Yuto Otaguro <yotagur@gmail.com>
2023-08-05 01:36:29 -0500

    Add files

---
+++ leetcode/1768/merge_strings_alternately.py
@@ -0,0 +1,5 @@
+def merge_strings_alternately():
+	pass
+
+if __name__ == '__main__':
+	print(merge_strings_alternately())
---
+++ leetcode/1768/merge_strings_alternately_test.py
@@ -0,0 +1,14 @@
+import unittest
+
+from merge_strings_alternately import merge_strings_alternately as foo
+
+class Testmerge_strings_alternately(unittest.TestCase):
+	def test_given_1(self):
+		pass
+
+	def test_given_2(self):
+		pass
+
+
+if __name__ == '__main__':
+    unittest.main()

0321a6452c5d7b20ea3256e5d692a2296d7639f0
Yuto Otaguro <yotagur@gmail.com>
2023-08-05 01:50:42 -0500

    Add files

---
+++ leetcode/27/remove_element.py
@@ -0,0 +1,5 @@
+def remove_element():
+	pass
+
+if __name__ == '__main__':
+	print(remove_element())
---
+++ leetcode/27/remove_element_test.py
@@ -0,0 +1,14 @@
+import unittest
+
+from remove_element import remove_element as foo
+
+class Testremove_element(unittest.TestCase):
+	def test_given_1(self):
+		pass
+
+	def test_given_2(self):
+		pass
+
+
+if __name__ == '__main__':
+    unittest.main()

6debdfd120a7af3cef233dc6ca6418d061c0bede
Yuto Otaguro <yotagur@gmail.com>
2023-08-05 01:48:58 -0500

    Done

--- leetcode/1768/merge_strings_alternately.py
+++ leetcode/1768/merge_strings_alternately.py
@@ -1,5 +1,17 @@
-def merge_strings_alternately():
-	pass
+def merge_strings_alternately(word1: str, word2: str) -> str:
+    i = 0
+    len1 = len(word1)
+    len2 = len(word2)
 
-if __name__ == '__main__':
-	print(merge_strings_alternately())
+    joined = ''
+
+    while i < min(len1, len2):
+        joined += word1[i] + word2[i]
+        i += 1
+
+    if i < len1:
+        joined += word1[i:]
+    else:
+        joined += word2[i:]
+
+    return joined

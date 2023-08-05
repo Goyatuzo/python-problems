b9ae3d2422edefe4c29bce52965d1cb8d97a2bbc
Yuto Otaguro <yotagur@gmail.com>
2023-08-05 02:11:47 -0500

    First attempt

--- leetcode/27/remove_element.py
+++ leetcode/27/remove_element.py
@@ -1,5 +1,18 @@
-def remove_element():
-	pass
+from typing import List
 
-if __name__ == '__main__':
-	print(remove_element())
+def remove_element(nums: List[int], val: int) -> int:
+    count = 0
+    for i in range(len(nums) - 1):
+        if nums[i] == val:
+            nums[i], nums[i + 1] = nums[i + 1], nums[i]
+        else:
+            count += 1
+
+
+    for n in nums:
+        if n != val:
+            count += 1
+
+    print(nums)
+
+    return count
--- leetcode/27/remove_element_test.py
+++ leetcode/27/remove_element_test.py
@@ -3,11 +3,16 @@ import unittest
 from remove_element import remove_element as foo
 
 class Test_Remove_Element(unittest.TestCase):
-	def test_given_1(self):
-        self.assertEqual(foo([2, 2, 3, 3], 3), 2)
+    def test_given_1(self):
+        t = [3, 2, 2, 3]
+        self.assertEqual(foo(t, 3), 2)
+        print(t)
 
-	def test_given_2(self):
-        self.assertEqual(foo([0, 1, 4, 0, 3, 2, 2, 2], 2), 5)
+    def test_given_2(self):
+        self.assertEqual(foo([0, 1, 2, 2, 3, 0, 4, 2], 2), 5)
+
+    def test_self_1(self):
+        self.assertEqual(foo([2, 2, 2, 3], 2), 1)
 
 
 if __name__ == '__main__':
